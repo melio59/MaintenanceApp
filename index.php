62e9fce332edeec6cdcba4ab86763a31353d48f9
Melvin Malagowski <133010333+melio59@users.noreply.github.com>
2024-03-06 10:26:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MaintenanceApp</title>
+</head>
+<body>
+    <div class="container">
+    <form class="formulaire" action="upload.php" method="post" enctype="multipart/form-data">
+        <h1>EPSILON</h1>
+        <p>Plateforme de peer-learning</p>
+        <p> EPSI </p>
+        <img src="./melvin.jfif" alt="logo" class="jokair">
+        <p> Melvin Malagowski </p>
+        
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <button type="submit" name="submit">Upload File</button>
+    </form>
+    </div>
+</body>
+</html>
